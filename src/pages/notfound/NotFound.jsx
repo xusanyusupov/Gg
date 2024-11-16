0a3bcf156322c83d155e6d17f1d99f747262a9f1
xusanyusupov <yusupov.xusan.org@gmail.com>
2024-11-16 19:12:23 +0500

    notfound

--- src/pages/notfound/NotFound.jsx
+++ src/pages/notfound/NotFound.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../notfound/notFound.scss";
 import { IoHome } from "react-icons/io5";
 import not from "../../assets/notfound.svg";
+import { NavLink } from "react-router-dom";
 
 const Notfound = () => {
   return (
@@ -10,7 +11,9 @@ const Notfound = () => {
         <img src={not} alt="" />
 
         <button class="not-found__btn">
-          <IoHome/> HOME
+        <NavLink to={'/'}>
+          Home
+        </NavLink>
         </button> 
       </div>
     </div>
